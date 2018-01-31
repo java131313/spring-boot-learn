d98dfce43f6759103df437f43cdabb4bc2efd671
System Administrator <root@kxys-MacBook-Pro.local>
2018-01-31 16:05:47 +0800

    dd

--- src/main/java/com/atguigu/crowdfunding/AtCrowdfundingApplication.java
+++ src/main/java/com/atguigu/crowdfunding/AtCrowdfundingApplication.java
@@ -3,10 +3,10 @@ package com.atguigu.crowdfunding;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 @EnableTransactionManagement
 @MapperScan("com.atguigu.**.dao")
 @ComponentScan(basePackages="com.atguigu")
@@ -14,11 +14,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class AtCrowdfundingApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(AtCrowdfundingApplication.class, args);
+		/*
+		SpringApplication application = new SpringApplication(AtCrowdfundingApplication.class, args);
+		application.setBannerMode(org.springframework.boot.Banner.Mode.OFF);
+		application.run(args);*/
 	}
 	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		builder.sources(AtCrowdfundingApplication.class);// Application为启动类
-		return super.configure(builder);
-	} 
+	
 }
